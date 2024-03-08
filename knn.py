84359699a2415899a4ab945243240214348d7026
Techmj <mrenaaljain@gmail.com>
2024-03-08 09:44:13 -0500

    knn working

--- knn.py
+++ knn.py
@@ -1,6 +1,3 @@
-print('hi')
-dadslk
-
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -15,7 +12,7 @@ from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_validate
 from sklearn.metrics import classification_report, confusion_matrix
 
-dataLungCancer = pd.read_csv('survey lung cancer.csv')
+dataLungCancer = pd.read_csv("survey lung cancer.csv")
 print(dataLungCancer.shape)
 print(dataLungCancer.head())
 
@@ -29,4 +26,29 @@ print(types)
 print("Keys of dataLungCancer dataset:\n", dataLungCancer.keys())
 
 # print the target variable
-print("Mood:", dataLungCancer['mood'])
+print("Lung Cancer:", dataLungCancer["LUNG_CANCER"])
+
+# Create the histogram of all the variables.
+dataLungCancer.hist(figsize=(20, 20))
+plt.show()
+
+# create a grid of scatter plot and histogram
+X = dataLungCancer[["YELLOW_FINGERS", "SMOKING", "ALCOHOL CONSUMING"]]
+y = dataLungCancer[["LUNG_CANCER"]]
+from pandas.plotting import scatter_matrix
+
+scatter_matrix(X, figsize=(10, 10))
+plt.show()
+
+
+# Creating a pairplot differentiated by Mood
+
+X = dataLungCancer[["YELLOW_FINGERS", "SMOKING", "ALCOHOL CONSUMING", "LUNG_CANCER"]]
+from pandas.plotting import scatter_matrix
+
+sns.pairplot(X, hue="LUNG_CANCER", kind = "kde")
+plt.show()
+
+
+# Check if null values in the columns
+print(X.isna().sum())
